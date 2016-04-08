11dbe12c5785af4fdaa2776a277e997a0983c470
Scott Lowe <scott.lowe@scottlowe.org>
2016-04-08 16:02:36 -0600

    Commit updated Terraform configuration
    Add cloud-config to configure CoreOS instances. Update vars.tf to account for new values (will need to be edited again for corporate environment). Update main.tf to specify 5 instances and leverage cloud-config. Update output.tf.

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,19 +1,24 @@
+# Image ID for CoreOS Stable 899.13.0 image
 variable "image" {
-	default = "<INSERT YOUR VALUE HERE>"
+    default = "6ebd5b1e-f5d5-4bf7-9ca8-713a2696307f"
 }
 
+# Default flavor to use for CoreOS instances
 variable "flavor" {
 	default = "m1.small"
 }
 
+# Network ID for external network to which new router will be connected
 variable "external_gateway" {
-	default = "<INSERT YOUR VALUE HERE>"
+	default = "c924f32c-a583-4b6a-af23-62746790a205"
 }
 
+# SSH keypair to be injected into new instances
 variable "key_pair" {
-	default = "<INSERT YOUR VALUE HERE>"
+	default = "lab"
 }
 
+# Network name for floating IP addresses (must correspond to external_gateway)
 variable "pool" {
-	default = "<INSERT YOUR VALUE HERE>"
+	default = "ext-net-5"
 }
