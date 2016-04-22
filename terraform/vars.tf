4d972787b352622f660be1a7477c80257258c303
Scott S. Lowe <scott.lowe@scottlowe.org>
2016-04-22 12:39:45 -0600

    Final changes to run in corporate lab
    Update vars.tf with correct information for corporate lab. Remove anti-affinity section from main.tf due to limited size of corporate lab.

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,6 +1,6 @@
 # Image ID for CoreOS Stable 899.13.0 image
 variable "image" {
-    default = "6ebd5b1e-f5d5-4bf7-9ca8-713a2696307f"
+    default = "6108ff8f-8cd5-447d-898b-b67bdc3eee17"
 }
 
 # Default flavor to use for CoreOS instances
@@ -10,15 +10,15 @@ variable "flavor" {
 
 # Network ID for external network to which new router will be connected
 variable "external_gateway" {
-	default = "c924f32c-a583-4b6a-af23-62746790a205"
+	default = "e7ef060a-5ff9-4148-8a38-993951ed9da9"
 }
 
 # SSH keypair to be injected into new instances
 variable "key_pair" {
-	default = "lab"
+	default = "aws"
 }
 
 # Network name for floating IP addresses (must correspond to external_gateway)
 variable "pool" {
-	default = "ext-net-5"
+	default = "EXTNET"
 }
